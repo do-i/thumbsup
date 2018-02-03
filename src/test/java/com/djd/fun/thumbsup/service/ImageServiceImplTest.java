e02a96acdb9f8d2b117e0ee7971b0606fa7c1c0f
Ninja <ninja@renton.net>
2018-02-03 15:15:27 -0600

    [mockito] Upgraded mockito to v2.x

--- src/test/java/com/djd/fun/thumbsup/service/ImageServiceImplTest.java
+++ src/test/java/com/djd/fun/thumbsup/service/ImageServiceImplTest.java
@@ -19,7 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static com.djd.fun.thumbsup.testutil.MoreMoreResources.getPathFromResouces;
 import static com.google.common.truth.Truth.assertThat;
